c613d8590ebdb6fa1f177916445740e8c5e94fb0
SumantNegi <sumant632000@gmail.com>
2021-09-04 15:15:27 +0530

    sumant second commit

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Product
+from .models import *
 admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(Order_items)
+admin.site.register(Ship_add)
--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+def brand(request):
+    products=Product.objects.all()
+    context={'products':products}
+    return render(request,'shop2.html',context)
+def product_detail(request):
+    return render(request,'product_d.html')
+def cart(request):
+    return render(request,'Cart.html')
+def checkout(request):
+    return render(request,'checkout.html')
 
 
 def contact(request):
@@ -16,5 +27,3 @@ def search(request):
     return HttpResponse("search page")
 def productview(request):
     return HttpResponse("productview page")
-def checkout(request):
-    return HttpResponse("checkout page")
